4cf805a618bfcd14007935865b021f81a6aa56d8
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-12-22 01:08:42 +0400

    fix: fix router bug

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,22 +1,23 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import type { ReactNode } from 'react';
 
+import { selectAuth } from '../store/slices/userSlice';
 import { GraphiQL, PageNotFound, SignUp, SingIn, Welcome } from '../components/pages';
 import Layout from '../components/widgets/Layout/Layout';
 import { useAppSelector } from '../hooks';
-import FallbackUI from '../components/shared/FallbackUI/FallbackUI.tsx';
+import FallbackUI from '../components/shared/FallbackUI/FallbackUI';
 
 type PrivateRouteProps = {
   element: ReactNode;
 };
 
 const PrivateMainRoute = ({ element }: PrivateRouteProps) => {
-  const isAuthenticated = useAppSelector((state) => state.user);
+  const { isAuthenticated } = useAppSelector(selectAuth);
   return isAuthenticated ? element : <Navigate to="/" />;
 };
 
 const PrivateSignRoute = ({ element }: PrivateRouteProps) => {
-  const isAuthenticated = useAppSelector((state) => state.user);
+  const { isAuthenticated } = useAppSelector(selectAuth);
   return !isAuthenticated ? element : <Navigate to="/main" />;
 };
 
