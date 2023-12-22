ff5805f1a94ecc751522a19c521c6e07b1e918f5
stroich <stroichmwork@gmail.com>
2023-12-22 15:49:39 +0400

    fix: fix DocsSection.tsx

--- src/components/widgets/DocsSection/DocsSection.tsx
+++ src/components/widgets/DocsSection/DocsSection.tsx
@@ -6,7 +6,7 @@ import { themeInput } from '../../../utils/themeInput/themeInput';
 
 import styles from './DocsSection.module.css';
 
-const DocumentationExplorer = lazy(() => import('../../features/DocumentationExplorer/DocumentationExplorer'));
+const DocumentationExplorer = lazy(() => import('../../features/DocumentationExplorer/DocumentationExplorer.tsx'));
 
 const DocsSection = () => {
   const translation = useTranslation();
@@ -20,7 +20,7 @@ const DocsSection = () => {
       <div className={styles.docsWrapper}>
         <button
           className={showDocumentation ? styles.buttonDocsOpen : styles.buttonDocs}
-          onClick={() => handleToggleDrawer(!showDocumentation)}
+          onClick={() => setShowDocumentation(!showDocumentation)}
         />
         <ThemeProvider theme={themeInput}>
           <TextField
